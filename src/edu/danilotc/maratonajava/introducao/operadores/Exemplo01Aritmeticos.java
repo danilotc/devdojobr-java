4e28168d6d76707ef90aae5f1605413984563b48
danilotc <danilotrindade87@gmail.com>
2023-06-13 12:16:13 -0300

    Aula 15 - Operadores pt 01 - Aritméticos

---
+++ src/edu/danilotc/maratonajava/introducao/operadores/Exemplo01Aritmeticos.java
@@ -0,0 +1,17 @@
+package edu.danilotc.maratonajava.introducao.operadores;
+
+public class Exemplo01Aritmeticos {
+    public static void main(String[] args) {
+        // entrada
+        int numero01 = 10;
+        int numero02 = 20;
+
+        // processamento
+        double resultado = numero01 / (double) numero02;
+        int resto = 15 % 2;
+
+        // saída
+        System.out.println(resultado);
+        System.out.println(resto);
+    }
+}
