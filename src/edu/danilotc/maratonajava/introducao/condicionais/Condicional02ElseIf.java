02671868abf3b3aaa9da4582de63a042b67fbb50
danilotc <danilotrindade87@gmail.com>
2023-06-16 03:57:06 -0300

    Aula 22 - Estruturas Condicionais pt 03 - Operador ternário

--- src/edu/danilotc/maratonajava/introducao/condicionais/Condicional02ElseIf.java
+++ src/edu/danilotc/maratonajava/introducao/condicionais/Condicional02ElseIf.java
@@ -13,6 +13,10 @@ public class Condicional02ElseIf {
             categoria = "Categoria Adulto";
         }
 
+        // Não usar operador ternário com mais de uma condição, apesar de possível, não é recomendado.
+        // Além disso, a instrução fica difícil de ler, diferente da mesma avaliação implementada acima.
+        categoria = idade < 15 ? "Categoria infantil" : idade >= 15 && idade < 18 ? "Categoria juvenil" : "Categorial adulto";
+
         System.out.println(categoria);
     }
 }
---
+++ src/edu/danilotc/maratonajava/introducao/condicionais/Condicional03Ternario.java
@@ -0,0 +1,9 @@
+package edu.danilotc.maratonajava.introducao.condicionais;
+
+public class Condicional03Ternario {
+    public static void main(String[] args) {
+        double salario = 6000;
+        String resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo!" : "Ainda não tenho condições, mas vou ter!";
+        System.out.println(resultado);
+    }
+}
