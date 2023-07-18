328162adff6b34997e2076865dc4beb73994a8d6
danilotc <danilotrindade87@gmail.com>
2023-07-18 14:21:49 -0300

    Aula 62 - Orientação Objetos - Métodos estáticos

--- README.md
+++ README.md
@@ -86,7 +86,7 @@ A. Os assuntos são esses aqui:
 1. [x] Maratona Java 59 - Orientação Objetos - Construtores pt 02 - Sobrecarga
 1. [x] Maratona Java 60 - Orientação Objetos - Blocos de inicialização
 1. [x] Maratona Java 61 - Orientação Objetos - Modificador static
-1. [ ] Maratona Java 62 - Orientação Objetos - Métodos estáticos
+1. [x] Maratona Java 62 - Orientação Objetos - Métodos estáticos
 1. [ ] Maratona Java 63 - Orientação Objetos - Bloco de inicialização estático
 1. [ ] Maratona Java 64 - Orientação Objetos - Associação pt 01 - Arrays com Objetos
 1. [ ] Maratona Java 65 - Orientação Objetos - Associação pt 02 - Associação unidirecional um para muitos
--- src/edu/danilotc/maratonajava/javacore/Fmodificadorestatico/dominio/Carro.java
+++ src/edu/danilotc/maratonajava/javacore/Fmodificadorestatico/dominio/Carro.java
@@ -4,7 +4,7 @@ public class Carro {
     private String nome;
     private double velocidadeMaxima;
 
-    public static double velocidadeLimite = 250;
+    private static double velocidadeLimite = 250;
 
     public Carro(String nome, double velocidadeMaxima) {
         this.nome = nome;
@@ -32,5 +32,13 @@ public class Carro {
 
     public void setVelocidadeMaxima(double velocidadeMaxima) {
         this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public static double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public static void setVelocidadeLimite(double velocidadeLimite) {
+        Carro.velocidadeLimite = velocidadeLimite;
     }
 }
