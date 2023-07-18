f2ebfdbd97763cd14dc53a145526966f1ad9523f
danilotc <danilotrindade87@gmail.com>
2023-07-18 15:11:24 -0300

    Aula 63 - Orientação Objetos - Bloco de inicialização estático

---
+++ src/edu/danilotc/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
@@ -0,0 +1,45 @@
+package edu.danilotc.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+
+    static {
+        System.out.println("Dentro do bloco de inicializacao estático 1");
+        episodios = new int[14];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicializacao estático 2");
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicializacao estático 3");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicializacao de instância não estático");
+    }
+
+    public Anime() {
+        for (int episodio : episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
