1c88cae7bd022811df05eac2c285f27563e5074b
Danilo Trindade <danilotrindade87@gmail.com>
2024-03-25 17:44:03 -0300

    Aula 80 - Orientação Objetos - Enumeração pt 01 - Introdução

---
+++ src/edu/danilotc/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -0,0 +1,32 @@
+package edu.danilotc.maratonajava.javacore.Kenum.dominio;
+
+public class Cliente {
+    private String nome;
+    private TipoCliente tipoCliente;
+
+    public Cliente(String nome, TipoCliente tipoCliente) {
+        this.nome = nome;
+        this.tipoCliente = tipoCliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{nome=" + this.getNome() + ", tipoCliente=" + this.getTipoCliente() + "}";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoCliente getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+}
---
+++ src/edu/danilotc/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -0,0 +1,5 @@
+package edu.danilotc.maratonajava.javacore.Kenum.dominio;
+
+public enum TipoCliente {
+    PESSOA_FISICA, PESSOA_JURIDICA
+}
