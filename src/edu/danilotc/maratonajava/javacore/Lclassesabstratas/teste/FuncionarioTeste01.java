67d7a936c079bf157900f9760890d24f707b1261
Danilo Trindade <danilotrindade87@gmail.com>
2024-03-28 16:36:48 -0300

    Aula 84 - Orientação Objetos - Classes abstratas pt 01

---
+++ src/edu/danilotc/maratonajava/javacore/Lclassesabstratas/teste/FuncionarioTeste01.java
@@ -0,0 +1,14 @@
+package edu.danilotc.maratonajava.javacore.Lclassesabstratas.teste;
+
+import edu.danilotc.maratonajava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import edu.danilotc.maratonajava.javacore.Lclassesabstratas.dominio.Funcionario;
+import edu.danilotc.maratonajava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTeste01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Florinda", 5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Chaves", 2000);
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
